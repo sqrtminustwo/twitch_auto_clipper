ca46763d83dd71ec3623e3e8e0f69725de9cedf3
mziborov <Maksym.Ziborov@UGent.be>
2026-09-07 01:00:57 +0200

    removed old dir

--- twitch_auto_clipper_sqrtminusone/type_aliases/types.py
+++
@@ -1,6 +0,0 @@
-from twitch_auto_clipper_sqrtminusone.clip.Clip import Clip
-
-from collections.abc import Callable
-from typing import TypeAlias
-
-OnClipCallBack: TypeAlias = Callable[[Clip], None]
