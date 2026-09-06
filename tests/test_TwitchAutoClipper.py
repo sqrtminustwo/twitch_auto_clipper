e4629fccef55abc9db9af43f2da5a8a6b4975cd7
mziborov <Maksym.Ziborov@UGent.be>
2026-09-06 22:50:26 +0200

    test_runner

--- tests/test_TwitchAutoClipper.py
+++ tests/test_TwitchAutoClipper.py
@@ -35,7 +35,6 @@ class TestTwitchAutoClipper(unittest.TestCase):
         patch.stopall()
 
     def test_init(self):
-        self.assertTrue(False)
         self.mock_tokens.assert_called_once_with(self.client_id, self.client_secret)
         self.tokens.initialize.assert_called_once()
         self.mock_streamer.assert_has_calls([call(name, ANY) for name in self.names])
